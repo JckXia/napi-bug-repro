a315c2de9a10cd33f05ac0b6692d30990ddb930a
Jack <32422811+JckXia@users.noreply.github.com>
2022-12-07 20:05:08 -0500

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,39 +1,12 @@
-{
-  "targets": [
-    {
-      "target_name": "binding",
-      "cflags!": [ "-fno-exceptions" ],
-      "cflags_cc!": [ "-fno-exceptions" ],
-      "sources": [ "electron_thread.cc"],
-      "include_dirs": [
-        "<!@(node -p \"require('node-addon-api').include\")"
-      ],
-       'dependencies': ["<!(node -p \"require('node-addon-api').gyp\")"],
-      'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
-       "cflags!" : [],
-       
-    "configurations": {
-        # This is used for generating code coverage with the `--debug` argument
-        "Debug": {
-          "conditions": [
-            ['OS=="linux"', {
-              "cflags": [],
-              "ldflags": []
-            }],
-            ['OS=="mac"', {
-              "xcode_settings": {
-                "OTHER_CFLAGS": [
-                  "-fprofile-arcs -ftest-coverage",
-                ],
-                "OTHER_LDFLAGS": [
-                ]
-              }
-            }]
 
-          ]
-        },
-    }
-    },
-    
-  ]
+const data = require('./build/Release/binding.node');
+ 
+
+function callEmit(arg)
+{
+    console.log(arg);
 }
+ 
+let a = data.CreateFunction({tsfn: callEmit});
+let objWrap = data.CreateObject();
+ 
